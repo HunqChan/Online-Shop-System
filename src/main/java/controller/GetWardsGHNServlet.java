5f2f0c9d1d04ce2383582434e5ed870f3e33bc30
toannqhe180972 <toannqhe180972@fpt.edu.vn>
2025-07-08 15:45:02 +0700

    Update GetWardsGHNServlet.java

--- src/main/java/controller/GetWardsGHNServlet.java
+++ src/main/java/controller/GetWardsGHNServlet.java
@@ -1,10 +1,10 @@
 package controller;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
