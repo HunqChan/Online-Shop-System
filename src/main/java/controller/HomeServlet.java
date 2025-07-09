2b41ea89d1932af752f82f92373768f679ffd763
toannqhe180972 <toannqhe180972@fpt.edu.vn>
2025-07-09 17:09:31 +0700

    Update HomeServlet.java

--- src/main/java/controller/HomeServlet.java
+++ src/main/java/controller/HomeServlet.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
+
 package controller;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -15,6 +12,7 @@ public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
         // Chuyển hướng đến trang home.jsp
         request.getRequestDispatcher("jsp/home.jsp").forward(request, response);
     }
